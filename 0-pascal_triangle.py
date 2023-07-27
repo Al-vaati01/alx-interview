cec7786e357225b1ed62f8ef975841a554ca6a40
Alvin <alvinvaati@gmail.com>
2023-07-27 21:55:48 +0300

    pascals traingle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    final = []
+    placeholder = 0
+    count = 0
+    if (n <= 0):
+        return []
+
+    while count < n:
+        placeholder = (11 ** count)
+        count += 1
+        final.append([int(a) for a in str(placeholder)])
+    for i in final:
+        print(i, sep="\n")
+
+
+pascal_triangle(1)
