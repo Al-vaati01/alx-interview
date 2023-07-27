d390aaed0e8f672ed5595edf6bbc79c8ae598b12
Alvin <alvinvaati@gmail.com>
2023-07-27 22:44:10 +0300

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
 def pascal_triangle(n):
     final = []
+    out = []
     placeholder = 0
     count = 0
     if (n <= 0):
@@ -8,6 +9,11 @@ def pascal_triangle(n):
     while count < n:
         placeholder = (11 ** count)
         count += 1
-        final.append([int(a) for a in str(placeholder)])
+        out = [int(i) for i in str(placeholder)]
+        convert = [int(i) for i in out]
 
-    return final
+        final.append(convert)
+    print(final)
+
+
+pascal_triangle(5)
