369c7d5597933bb29ac8506cdd0f5e3471a87fc9
Alvin <alvinvaati@gmail.com>
2023-08-03 16:29:59 +0300

    lockboxes file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a method that determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    def search(box_index, visited):
+        visited[box_index] = True
+        for key in boxes[box_index]:
+            if not visited[key]:
+                search(key, visited)
+
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+
+    search(0, visited)
+
+    return all(visited)
