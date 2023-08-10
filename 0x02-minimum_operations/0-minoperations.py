baa69e7af09144848046068aeb79597b78e8ef7f
Alvin <alvinvaati@gmail.com>
2023-08-11 01:19:32 +0300

    format

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,18 +5,6 @@ Operations module
 
 
 def minOperations(n):
-    """
-    a method that calculates the fewest number of operations
-    needed to result in exactly n H characters in the file.
-
-    Args:
-        n (int): The target number of H characters.
-
-    Returns:
-        int: The minimum number of operations required to achieve
-        'n' H characters.
-             Returns 0 if 'n' is impossible to achieve.
-    """
     if n <= 1:
         return 0
 
