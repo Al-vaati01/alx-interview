daa8bb629e2dbcba7b20d05c39b8eda95961670e
Alvin <alvinvaati@gmail.com>
2023-09-07 12:32:58 +0300

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (error, response, body) => {
-    if (error) {
+request(
+    `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+    (error, response, body) => {
+      if (error) {
         console.error(error);
         return;
-    }
+      }
 
-    // Assuming the response body is a JSON string, parse it into an object
-    const data = JSON.parse(body);
+      // Assuming the response body is a JSON string, parse it into an object
+      const data = JSON.parse(body);
 
-    if (data && data.hasOwnProperty("characters")) {
+      if (data && 'characters' in data) {
         data.characters.forEach((character) => {
-            request(character, (error, response, body) => {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
-                const data = JSON.parse(body);
-                console.log(data.name);
-            })
+          request(character, (error, response, body) => {
+            if (error) {
+              console.error(error);
+              return;
+            }
+            const data = JSON.parse(body);
+            console.log(data.name);
+          });
         });
-    } else {
+      } else {
         console.log("No 'characters' property found in the response.");
+      }
     }
-});
+);
