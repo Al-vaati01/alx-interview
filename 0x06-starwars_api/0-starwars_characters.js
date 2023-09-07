9931518245f1829b5975b2341974d1def1367691
Alvin <alvinvaati@gmail.com>
2023-09-07 13:19:21 +0300

    some restructuring

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,45 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(
-    `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
-    (error, _, body) => {
+function makeRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
       if (error) {
-        console.error(error);
-        return;
-      }
-
-      // Assuming the response body is a JSON string, parse it into an object
-      const data = JSON.parse(body);
-
-      if (data && 'characters' in data) {
-        data.characters.forEach((character) => {
-          request(character, (error, _, body) => {
-            if (error) {
-              console.error(error);
-              return;
-            }
-            const data = JSON.parse(body);
-            console.log(data.name);
-          });
-        });
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(error, `${response.statusCode}`);
       } else {
-        console.log("No 'characters' property found in the response.");
+        resolve(body);
       }
+    });
+  });
+}
+
+async function fetchFilmCharacters (filmId) {
+  try {
+    const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+    const filmBody = await makeRequest(filmUrl);
+    const filmData = JSON.parse(filmBody);
+
+    if (!filmData || !filmData.characters || filmData.characters.length === 0) {
+      console.log('No characters found in the response.');
+      return;
     }
-);
+
+    for (const characterUrl of filmData.characters) {
+      const characterBody = await makeRequest(characterUrl);
+      const characterData = JSON.parse(characterBody);
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const filmId = process.argv[2];
+
+if (!filmId) {
+  console.error('Please provide a film ID as a command-line argument.');
+} else {
+  fetchFilmCharacters(filmId);
+}
